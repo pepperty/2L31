032ef16798ab93da71dd4dcba95797aa25654af6
Tiradate <114634898+Tiradate@users.noreply.github.com>
2023-09-19 00:17:19 +0700

    error and back

--- 2L31_U2F2D2Pub_TA.py
+++ 2L31_U2F2D2Pub_TA.py
@@ -31,10 +31,6 @@ import os
 import time
 import statistics
 
-# log
-import csv
-import datetime
-
 
 Alast = 0
 Blast = 0
@@ -100,16 +96,6 @@ def measure(IO_TRIG,IO_EXC):
     else:
         return -1
     return distance
-
-def get_cpu_temperature():
-    try:
-        result = subprocess.check_output(["vcgencmd", "measure_temp"])
-        temperature_str = result.decode("utf-8")
-        temperature = float(temperature_str.split("=")[1].split("'")[0])
-        return temperature
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
-        return None
 # Stop Node-RED
 # if platform.system() == "Windows":
 #     node_red_path = r'C:\Users\peppe\AppData\Roaming\npm\node-red.cmd'
@@ -203,123 +189,79 @@ with open('/home/DEV01/2L31/2L31_BPU_TA.json', 'r') as file:
     json_data1 = json.load(file)
 # print(json_data)
 if __name__ == '__main__':
-    while True:
-        try:
-            def connect(host='http://google.com'):
-                try:
-                    urllib.request.urlopen(host) #Python 3.x
-                    return True
-                except:
-                    return False
-            # Function to log the current timestamp
-            def log_timestamp():
-                current_time = datetime.datetime.now()
-                return current_time.strftime("%Y-%m-%d %H:%M:%S")
-            # Create a list to store log data
-            log_data = []
+    try:
+        while True:
+            time.sleep(1)
+            dist_A_list = []
+            dist_B_list = []
+            for i in range(10):
+                time.sleep(0.5)
+                Anow = measure(IO_TRIG01,IO_EXC01)
+                if(Anow > -1):
+                    #if (Anow - Alast <50 and Anow - Alast > -50) or Alast == 0:
+                    dist_A_list.append(Anow)
+                    Alast = Anow
+                else:
+                    dist_A_list.append(0)
+                    print("#UA")
+                    
+                time.sleep(0.5)
+                Bnow = measure(IO_TRIG02,IO_EXC02)
+                if(Bnow > -1):
+                    #if (Bnow - Blast <50 and Bnow - Blast > -50) or Blast == 0:
+                    dist_B_list.append(Bnow)
+                    Blast = Bnow
+                else:
+                    dist_B_list.append(0)
+                    print("#UB")
+                    
+            if len(dist_A_list)>0:
+                dist_A = round(min(dist_A_list),3)
+            if len(dist_B_list)>0:
+                dist_B = round(min(dist_B_list),3)
 
-            # Generate log data (you can do this whenever you want to log data)
-            log_data.append(log_timestamp())
+            # Read the state of the GPIO pin
+            DET01_state = GPIO.input(IO_DET01)
+            DET02_state = GPIO.input(IO_DET02)
+            print("################")
+            print(Anow)
+            print(Bnow)
+            print(dist_A)
+            print(dist_B)
+            print(DET01_state)
+            print(DET02_state)
+            print("################")
 
-            # CSV filename
-            csv_filename = "timestamps.csv"
-
-            # Check if the file already exists
-            file_exists = True
-            try:
-                with open(csv_filename, "r") as file:
-                    reader = csv.reader(file)
-                    if not list(reader):
-                        file_exists = False
-            except FileNotFoundError:
-                file_exists = False
-
-            # If the file doesn't exist, create it with a header row
-            if not file_exists:
-                with open(csv_filename, mode="w", newline="") as file:
-                    writer = csv.writer(file)
-                    ft_raw = ["Timestamp","ping","alarm","trouble"]
-                    writer.writerow(ft_raw)  # Header row
-
-            while connect():
-                time.sleep(1)
-                dist_A_list = []
-                dist_B_list = []
-                for i in range(10):
-                    time.sleep(0.5)
-                    Anow = measure(IO_TRIG01,IO_EXC01)
-                    if(Anow > -1):
-                        #if (Anow - Alast <50 and Anow - Alast > -50) or Alast == 0:
-                        dist_A_list.append(Anow)
-                        Alast = Anow
+            if mode == 'both' or mode == 'publish':
+                # print("################")
+                # print(json_data1['devices'][0]['tags'][2])
+                # print("################")
+                trMill = int(time.time())
+                if (trMill-tlMill)>30:
+                    # Print the state (it should read LOW when the button is not pressed)
+                    if DET01_state == GPIO.LOW:
+                        json_data1['devices'][0]['tags'][0]['value'] = "Leak"
                     else:
-                        dist_A_list.append(0)
-                        print("#UA")
-                        
-                    time.sleep(0.5)
-                    Bnow = measure(IO_TRIG02,IO_EXC02)
-                    if(Bnow > -1):
-                        #if (Bnow - Blast <50 and Bnow - Blast > -50) or Blast == 0:
-                        dist_B_list.append(Bnow)
-                        Blast = Bnow
+                        json_data1['devices'][0]['tags'][0]['value'] = "Nomal"
+                    if DET02_state == GPIO.LOW:
+                        json_data1['devices'][1]['tags'][0]['value'] = "Leak"
                     else:
-                        dist_B_list.append(0)
-                        print("#UB")
-                        
-                if len(dist_A_list)>0:
-                    dist_A = round(min(dist_A_list),3)
-                if len(dist_B_list)>0:
-                    dist_B = round(min(dist_B_list),3)
+                        json_data1['devices'][1]['tags'][0]['value'] = "Nomal"
 
-                # Read the state of the GPIO pin
-                DET01_state = GPIO.input(IO_DET01)
-                DET02_state = GPIO.input(IO_DET02)
-                print("################")
-                print(Anow)
-                print(Bnow)
-                print(dist_A)
-                print(dist_B)
-                print(DET01_state)
-                print(DET02_state)
-                print("################")
+                    json_data1['devices'][0]['tags'][1]['value'] = dist_A
+                    json_data1['devices'][1]['tags'][1]['value'] = dist_B
 
-                if mode == 'both' or mode == 'publish':
-                    # print("################")
-                    # print(json_data1['devices'][0]['tags'][2])
-                    # print("################")
-                    trMill = int(time.time())
-                    if (trMill-tlMill)>30:
-                        # Print the state (it should read LOW when the button is not pressed)
-                        if DET01_state == GPIO.LOW:
-                            json_data1['devices'][0]['tags'][0]['value'] = "Leak"
-                        elif DET01_state == GPIO.HIGH:
-                            json_data1['devices'][0]['tags'][0]['value'] = "Nomal"
-                        elif DET02_state == GPIO.LOW:
-                            json_data1['devices'][1]['tags'][0]['value'] = "Leak"
-                        elif DET02_state == GPIO.HIGH:
-                            json_data1['devices'][1]['tags'][0]['value'] = "Nomal"
+                    messageJson1 = json.dumps(json_data1)
+                    myAWSIoTMQTTClient.publish(topic, messageJson1, 0)
+                    tlMill = int(time.time())
 
-                        json_data1['devices'][0]['tags'][1]['value'] = dist_A
-                        json_data1['devices'][1]['tags'][1]['value'] = dist_B
-
-                        messageJson1 = json.dumps(json_data1)
-                        myAWSIoTMQTTClient.publish(topic, messageJson1, 0)
-                        tlMill = int(time.time())
-
-                        # if mode == 'publish':
-                        #     print('Published topic %s: %s\n' % (topic, messageJson1))
-            # Reset by pressing CTRL + C
-            time.sleep(30)
-            # Append log data to the CSV file
-            log_data.append(connect())
-            log_data.append(json_data1['devices'][0]['tags'][0]['value'])
-            log_data.append(json_data1['devices'][1]['tags'][0]['value'])
-            with open(csv_filename, mode="a", newline="") as file:
-                writer = csv.writer(file)
-                writer.writerow(log_data)
-        except KeyboardInterrupt:
-            print("Measurement stopped by User")
-            GPIO.cleanup()
-        except Exception as e:
-            print(f"An exception occurred: {str(e)}")
-            pass
+                    if mode == 'publish':
+                        print('Published topic %s: %s\n' % (topic, messageJson1))
+        # Reset by pressing CTRL + C
+    except KeyboardInterrupt:
+        print("Measurement stopped by User")
+        GPIO.cleanup()
+    # except Exception as e:
+    #     print(f"An error occurred: {e}")
+    #     GPIO.cleanup()
+    GPIO.cleanup()
